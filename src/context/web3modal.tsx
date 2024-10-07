e1e5ea395fd26c9f758fa628fb2cf4f8905f20ce
Coderdivine <chimdi4332@gmail.com>
2024-10-07 09:11:19 +0100

    EVM

--- src/context/web3modal.tsx
+++ src/context/web3modal.tsx
@@ -1,15 +1,13 @@
 "use client";
-import { createAppKit } from "@reown/appkit/react";
-import dotenv from "dotenv";
-dotenv.config();
-import { EthersAdapter } from "@reown/appkit-adapter-ethers";
-import { NETWORKS } from "@/configs/networks";
-import { arbitrum, mainnet } from "@reown/appkit/networks";
-const projectId = process.env.NEXT_PROJECT_ID || "b6307caf0557efa438a85b8aa60cd87a";
-if (!projectId) {
-  throw new Error('Project ID is not defined')
-}
-const defaultNetwork = NETWORKS.assetchain_mainnet;
+// import { createAppKit } from "@reown/appkit/react";
+// import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+// import { NETWORKS } from "@/configs/networks";
+// import { arbitrum, mainnet } from "@reown/appkit/networks";
+// const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+// if (!projectId) {
+//   throw new Error('Project ID is not defined')
+// }
+// const defaultNetwork = NETWORKS.assetchain_mainnet;
 
 const metadata = {
   name: "Starter kit",
@@ -18,18 +16,6 @@ const metadata = {
   icons: ["https://avatars.githubusercontent.com/u/37784886"]
 };
 
-
-
-createAppKit({
-  adapters: [new EthersAdapter()],
-  metadata,
-  //@ts-ignore
-  defaultNetwork,
-  projectId,
-  features: {
-    analytics: false,
-  },
-});
 
 export default function AppKit({
   children,
